cd8814bf243db1bb91617418df5848cb82ac4d6a
Matthew Torrence <matt@torrencefamily.net>
2019-12-15 15:44:51 -0500

    Add arithmetic builtins

--- src/solver/builtins.rs
+++ src/solver/builtins.rs
@@ -10,6 +10,16 @@ pub fn builtins() -> HashMap<String, Builtin> {
     let mut res = HashMap::new();
     res.insert("=".to_string(), 
                builtin_eq as Builtin);
+    res.insert("+".to_string(),
+               builtin_add as Builtin);
+    res.insert("-".to_string(),
+               builtin_sub as Builtin);
+    res.insert("*".to_string(),
+               builtin_mul as Builtin);
+    res.insert("/".to_string(),
+               builtin_div as Builtin);
+    res.insert("%".to_string(),
+               builtin_mod as Builtin);
     res
 }
 
@@ -20,3 +30,198 @@ pub fn builtin_eq(cterm: &CompoundTerm, _state: &SolverState) -> Option<Unifier>
     compute_most_gen_unifier(vec![(a, b)])
 }
 
+pub fn builtin_add(cterm: &CompoundTerm, _state: &SolverState) -> Option<Unifier> {
+    assert!(cterm.args.len() == 3);
+    let a = cterm.args[0].clone();
+    let b = cterm.args[1].clone();
+    let c = cterm.args[2].clone();
+    match (a, b, c) {
+        (Term::Unknown(s),
+        Term::Number(a),
+        Term::Number(b)) => {
+            let mut unif = Unifier::new();
+            unif.insert(s.clone(), Term::Number(b - a));
+            Some(unif)
+        },
+        (Term::Number(a),
+        Term::Unknown(s),
+        Term::Number(b)) => {
+            let mut unif = Unifier::new();
+            unif.insert(s.clone(), Term::Number(b - a));
+            Some(unif)
+        },
+        (Term::Number(a),
+        Term::Number(b),
+        Term::Unknown(s)) => {
+            let mut unif = Unifier::new();
+            unif.insert(s.clone(), Term::Number(a + b));
+            Some(unif)
+        },
+        (Term::Number(a),
+        Term::Number(b),
+        Term::Number(c)) => {
+            if a + b == c {
+                Some(Unifier::new())
+            } else {
+                None
+            }
+        },
+        _ => {
+            panic!("arguments to arithmetic not defined enough! \nthis will be a non-fatal error in the future")
+        }
+    }
+}
+
+pub fn builtin_sub(cterm: &CompoundTerm, _state: &SolverState) -> Option<Unifier> {
+    assert!(cterm.args.len() == 3);
+    let a = cterm.args[0].clone();
+    let b = cterm.args[1].clone();
+    let c = cterm.args[2].clone();
+    match (a, b, c) {
+        (Term::Unknown(s),
+        Term::Number(a),
+        Term::Number(b)) => {
+            let mut unif = Unifier::new();
+            unif.insert(s.clone(), Term::Number(b + a));
+            Some(unif)
+        },
+        (Term::Number(a),
+        Term::Unknown(s),
+        Term::Number(b)) => {
+            let mut unif = Unifier::new();
+            unif.insert(s.clone(), Term::Number(a - b));
+            Some(unif)
+        },
+        (Term::Number(a),
+        Term::Number(b),
+        Term::Unknown(s)) => {
+            let mut unif = Unifier::new();
+            unif.insert(s.clone(), Term::Number(a - b));
+            Some(unif)
+        },
+        (Term::Number(a),
+        Term::Number(b),
+        Term::Number(c)) => {
+            if a - b == c {
+                Some(Unifier::new())
+            } else {
+                None
+            }
+        },
+        _ => {
+            panic!("arguments to arithmetic not defined enough! \nthis will be a non-fatal error in the future")
+        }
+    }
+}
+
+pub fn builtin_mul(cterm: &CompoundTerm, _state: &SolverState) -> Option<Unifier> {
+    assert!(cterm.args.len() == 3);
+    let a = cterm.args[0].clone();
+    let b = cterm.args[1].clone();
+    let c = cterm.args[2].clone();
+    match (a, b, c) {
+        (Term::Unknown(s),
+        Term::Number(a),
+        Term::Number(b)) => {
+            let mut unif = Unifier::new();
+            unif.insert(s.clone(), Term::Number(b / a));
+            Some(unif)
+        },
+        (Term::Number(a),
+        Term::Unknown(s),
+        Term::Number(b)) => {
+            let mut unif = Unifier::new();
+            unif.insert(s.clone(), Term::Number(b / a));
+            Some(unif)
+        },
+        (Term::Number(a),
+        Term::Number(b),
+        Term::Unknown(s)) => {
+            let mut unif = Unifier::new();
+            unif.insert(s.clone(), Term::Number(a * b));
+            Some(unif)
+        },
+        (Term::Number(a),
+        Term::Number(b),
+        Term::Number(c)) => {
+            if a * b == c {
+                Some(Unifier::new())
+            } else {
+                None
+            }
+        },
+        _ => {
+            panic!("arguments to arithmetic not defined enough! \nthis will be a non-fatal error in the future")
+        }
+    }
+}
+
+pub fn builtin_div(cterm: &CompoundTerm, _state: &SolverState) -> Option<Unifier> {
+    assert!(cterm.args.len() == 3);
+    let a = cterm.args[0].clone();
+    let b = cterm.args[1].clone();
+    let c = cterm.args[2].clone();
+    match (a, b, c) {
+        (Term::Unknown(s),
+        Term::Number(a),
+        Term::Number(b)) => {
+            let mut unif = Unifier::new();
+            unif.insert(s.clone(), Term::Number(b * a));
+            Some(unif)
+        },
+        (Term::Number(a),
+        Term::Unknown(s),
+        Term::Number(b)) => {
+            let mut unif = Unifier::new();
+            unif.insert(s.clone(), Term::Number(a / b));
+            Some(unif)
+        },
+        (Term::Number(a),
+        Term::Number(b),
+        Term::Unknown(s)) => {
+            let mut unif = Unifier::new();
+            unif.insert(s.clone(), Term::Number(a / b));
+            Some(unif)
+        },
+        (Term::Number(a),
+        Term::Number(b),
+        Term::Number(c)) => {
+            if a / b == c {
+                Some(Unifier::new())
+            } else {
+                None
+            }
+        },
+        _ => {
+            panic!("arguments to arithmetic not defined enough! \nthis will be a non-fatal error in the future")
+        }
+    }
+}
+
+pub fn builtin_mod(cterm: &CompoundTerm, _state: &SolverState) -> Option<Unifier> {
+    assert!(cterm.args.len() == 3);
+    let a = cterm.args[0].clone();
+    let b = cterm.args[1].clone();
+    let c = cterm.args[2].clone();
+    match (a, b, c) {
+        (Term::Number(a),
+        Term::Number(b),
+        Term::Unknown(s)) => {
+            let mut unif = Unifier::new();
+            unif.insert(s.clone(), Term::Number(a % b));
+            Some(unif)
+        },
+        (Term::Number(a),
+        Term::Number(b),
+        Term::Number(c)) => {
+            if a % b == c {
+                Some(Unifier::new())
+            } else {
+                None
+            }
+        },
+        _ => {
+            panic!("arguments to arithmetic not defined enough! \nthis will be a non-fatal error in the future")
+        }
+    }
+}
